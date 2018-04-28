2460959b7b973ca782dc7c1867e9b5e4781ca509
keyur2714 <keyur.555kn@gmail.com>
2018-04-28 14:21:14 +0530

    Updated Success message component

--- BasicsDemo/src/app/message/message.component.ts
+++ BasicsDemo/src/app/message/message.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
+  successMessage: string = "Success Message";
+
   constructor() { }
 
   ngOnInit() {
