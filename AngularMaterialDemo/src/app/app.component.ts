337a076b501c4406a0f51c36cc32331a1e7e6123
keyur2714 <keyur.555kn@gmail.com>
2018-07-21 10:25:25 +0530

    Added Dialog Example

--- AngularMaterialDemo/src/app/app.component.ts
+++ AngularMaterialDemo/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component,ViewChild,Inject } from '@angular/core';
 import {MatTableDataSource,MatPaginator} from '@angular/material';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']  
 })
 export class AppComponent {
   title = 'Material Components Demo';
@@ -30,6 +31,7 @@ export class AppComponent {
   ];
   dataSource = new MatTableDataSource(this.departments);
   
+  constructor(public dialog: MatDialog){}
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
@@ -39,4 +41,35 @@ export class AppComponent {
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
+
+  openDialog(){
+    console.log("Open Dialog...!");
+
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: 'keyur'}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');     
+
+    });
+
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: string) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
--- AngularMaterialDemo/src/app/app.module.ts
+++ AngularMaterialDemo/src/app/app.module.ts
@@ -6,12 +6,18 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent,DialogOverviewExampleDialog } from './app.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    DialogOverviewExampleDialog
+  ],
+  entryComponents: [
     AppComponent
   ],
   imports: [
@@ -21,9 +27,10 @@ import { AppComponent } from './app.component';
     MatTableModule,
     MatInputModule,
     MatPaginatorModule,
+    MatDialogModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MatDialog],
+  bootstrap: [AppComponent,DialogOverviewExampleDialog]
 })
 export class AppModule { }
